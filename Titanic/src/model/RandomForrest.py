c67942fd71bbbb474b48d10949cebf1f6e75035d
Rashaad Cassim <rashcassim@gmail.com>
2021-08-31 18:00:32 +0200

    Adapted RF to new Util class

--- Titanic/src/model/RandomForrest.py
+++ Titanic/src/model/RandomForrest.py
@@ -1,23 +1,25 @@
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, accuracy_score
 import pandas as pd
 from Titanic.src.Util.Util import Util
 from Titanic.src.Util.Util import get_validation_curve
 from Titanic.src.Util.Util import find_optimal_param
-from Titanic.src.Util.Util import scale_df
 
 # TODO Adapt file to refactor of Util Class: moving scale_df inside class, refactor of getdf and re optimise for
 #  new Features
 def main():
     util = Util()
-    [X, y] = util.get_df('/train.csv')
-    X = X[util.configValues['columns']]
-    X =scale_df(X)
+    [X, y, X_test] = util.get_df()
+    X.drop('Survived', inplace=True, axis=1)
+    X_test.drop('Survived', inplace=True, axis=1)
+
+    print("Columns used: ")
+    print(X.columns)
+
     y = np.ravel(y)
-    X_test = util.get_df('/test.csv', False)
-    X_test = X_test[util.configValues['columns']]
-    X_test = scale_df(X_test)
+    X = util.scale_df(X)
+    X_test = util.scale_df(X_test)
     df = pd.read_csv('../../data/test.csv')
 
     n_estimators = [100, 150, 300, 500]
@@ -49,19 +51,10 @@ def main():
     if util.configValues['predict']:
 
         # Use optimal Model
-        params = {'max_depth': 7, 'min_samples_leaf': 1, 'min_samples_split': 2, 'n_estimators': 100,
-                  'random_state': 100, 'max_features': 'auto', 'criterion': 'entropy'}
+        params = {'max_depth': 20, 'min_samples_leaf': 1, 'min_samples_split': 2, 'n_estimators': 150,
+                  'random_state': 40, 'max_features': 'auto', 'criterion': 'entropy'}
 
         overall_predictions = {}
-        # [X, y, x_cv, y_cv] = util.get_df('/train.csv', True, True)
-        # X = X[util.configValues['columns']]
-        # x_cv = x_cv[util.configValues['columns']]
-        #
-        # X = scale_df(X)
-        # # x_cv = scale_df(x_cv)
-
-        # X.drop('Survived', inplace=True, axis=1)
-        # x_cv.drop('Survived', inplace=True, axis=1)
         f1 = 0
         for i in range(30):
             model = RandomForestClassifier(**params)
@@ -70,11 +63,19 @@ def main():
             predictions = model.predict(X)
             f1_iter = f1_score(y, predictions)
             if f1_iter > f1:
-                overall_predictions = model.predict(X_test)
+                overall_predictions = model.predict(X_test).astype(int)
                 f1 = f1_iter
         print("f1 score on train data: %s" % str(f1))
+        input_predictions = model.predict(X).astype(int)
+        accuracy = accuracy_score(y, input_predictions)
+
+        print("|     accuracy score on train data: %s     |" % str(accuracy))
         print(overall_predictions.sum()/len(overall_predictions))
-        print(y.sum()/len(y))
+        print("|     Percentage Survived training set %s   |" % str(y.sum() / len(y)))
+        print("|     Predicted Percentage Survived training set %s   |" % str(input_predictions.sum()
+                                                                             / len(input_predictions)))
+        print(
+            "|    Percentage Survived test set %s       |" % str(overall_predictions.sum() / len(overall_predictions)))
         output = pd.DataFrame({'PassengerId': df.PassengerId, 'Survived': overall_predictions})
         output.to_csv('../../predictions/RandomForrestOptimized.csv', index=False)
         print("Your submission was successfully saved!")
